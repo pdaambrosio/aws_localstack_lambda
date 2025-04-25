1cfab0ffd93b84aca346cbfe421924e94f96c0cb
pdambro <pdambro@mapfre.net>
2025-04-25 13:43:53 -0300

    feat: update init-secrets.sh to use local endpoint and test credentials

--- init-secrets.sh
+++ init-secrets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-awslocal secretsmanager create-secret \
+aws --endpoint-url=http://localhost:4566 secretsmanager create-secret \
   --name aws/assume-role/creds \
-  --secret-string '{"aws_access_key_id":"dummy-access-key", "aws_secret_access_key":"dummy-secret-key"}'
+  --secret-string '{"aws_access_key_id":"test-access-key","aws_secret_access_key":"test-secret-key"}'
